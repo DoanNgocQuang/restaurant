b5fa5ceb9bfd477eef78173a82a8dec6dbdf05a8
Duy <nam05hnvn@gmail.com>
2026-04-10 10:43:04 +0700

    Add order, system log, revenue statistic

--- be/src/main/java/com/ngocquang/restautant/modules/booking/repository/BookingRepository.java
+++ be/src/main/java/com/ngocquang/restautant/modules/booking/repository/BookingRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ngocquang.restautant.modules.booking.entity.Booking;
+import com.ngocquang.restautant.modules.user.entity.User;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
@@ -15,4 +16,6 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     boolean existsByTables_IdAndStatusIn(Integer tableId, Collection<Booking.Status> statuses);
 
     boolean existsByTables_IdAndStatusInAndIdNot(Integer tableId, Collection<Booking.Status> statuses, Integer id);
+
+    List<Booking> findByUserAndStatusOrderByBookingTimeDesc(User user, Booking.Status status);
 }
